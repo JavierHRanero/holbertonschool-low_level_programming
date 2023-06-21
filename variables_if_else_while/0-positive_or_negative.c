b21c9a8c37464fc267989d453a344771f7404b94
JavierHRanero <javierherreraranero@gmail.com>
2023-06-21 11:35:01 -0700

    0.Cualquier cosa positiva

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* main - Entry point of the program 
- *
- *Description: This program assigns a random number to the variable n 
- *             and prints whether the number is positive, negative, or zero.
- *
- *Return : 0 on success
- */
+
+/*
+* main - Entry point of the program 
+*
+*Description: This program assigns a random number to the variable n 
+*             and prints whether the number is positive, negative, or zero.
+*
+*Return : 0 on success
+*/
 int main(void)
 {
 int n;
