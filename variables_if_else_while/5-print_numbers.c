f54f1e568e5299124a2f321f17ddf9cd3b58a891
JavierHRanero <javierherreraranero@gmail.com>
2023-06-21 23:19:48 -0700

    5.Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+*Description: This progra assigns a random number to the variable n
+*and prints whether the number is positive, negative, or zero.
+*
+*Return: 0 on success
+*/
+int main(void)
+{
+for (int numero = 0; numero <= 9; numero ++)
+{
+putchar('0' + numero);
+}
+putchar('\n');
+return (0);
+}
