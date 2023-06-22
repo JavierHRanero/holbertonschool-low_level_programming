e54e03dce1613c525e21a80e840d820b0d8d2361
JavierHRanero <javierherreraranero@gmail.com>
2023-06-21 23:01:50 -0700

    2.I sometimes suffer from insomnia

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 *main - Entry point of the program
 *
 *Description: This progra assigns a random number to the variable n
-*and prints whether the number is positive, negative, or zero.  
+*and prints whether the number is positive, negative, or zero.
 *
 *Return: 0 on success
 */
