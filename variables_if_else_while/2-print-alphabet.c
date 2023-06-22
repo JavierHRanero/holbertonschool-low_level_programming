212dbb75c7e9f85e1cefa7b68fc868c956637d06
JavierHRanero <javierherreraranero@gmail.com>
2023-06-21 22:55:05 -0700

    2.I sometimes suffer from insomnia

---
+++ variables_if_else_while/2-print-alphabet.c
@@ -0,0 +1,193 @@
+#include <stdlib.h>
+  
+#include <time.h>
+
+#include <stdio.h>
+
+/**                                                                                                                                     
+
+ *main - Entry point of the program                                                                                                      
+
+ *                                                                                                                                       
+
+ *Description: This progra assigns a random number to the variable n                                                                     
+
+ *and prints whether the number is positive, negative, or zero.                                                                          
+
+ *                                                                                                                                       
+
+ *Return: 0 on success                                                                                                                   
+
+ */
+
+int main(void)
+  
+{
+  
+  int n;
+  
+  int ultimoDigito;
+  
+  srand(time(0));
+  
+  n = rand() - RAND_MAX / 2;
+  
+  ultimoDigito = n % 10;
+  
+  printf("Last digit of %d is %d ", n, ultimoDigito);
+  
+  if (ultimoDigito > 5)
+    
+    {
+      
+      printf("and is greater than 5\n");
+      
+    }
+  
+  else if (ultimoDigito == 0)
+    
+    {
+      
+      printf("and is 0\n");
+      
+    }
+  
+  else
+    
+    {
+      
+      printf("and is less than 6 and not 0\n");
+      
+    }
+  
+  return (0);
+  
+}#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+ /**                                                                                                                                     
+
+  *main - Entry point of the program                                                                                                      
+
+  *                                                                                                                                       
+
+  *Description: This progra assigns a random number to the variable n                                                                     
+
+  *and prints whether the number is positive, negative, or zero.                                                                          
+
+  *                                                                                                                                       
+
+  *Return: 0 on success                                                                                                                   
+
+  */
+
+int main(void)
+
+{
+  
+  int n;
+  
+  int ultimoDigito;
+  
+  srand(time(0));
+  
+  n = rand() - RAND_MAX / 2;
+  
+  ultimoDigito = n % 10;
+  
+  printf("Last digit of %d is %d ", n, ultimoDigito);
+  
+  if (ultimoDigito > 5)
+    
+    {
+      
+      printf("and is greater than 5\n");
+      
+    }
+  
+  else if (ultimoDigito == 0)
+    
+    {
+      
+      printf("and is 0\n");
+      
+    }
+  
+  else
+    
+    {
+      
+      printf("and is less than 6 and not 0\n");
+      
+    }
+  
+  return (0);
+  
+}#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+ /**                                                                                                                                     
+
+  *main - Entry point of the program                                                                                                      
+
+  *                                                                                                                                       
+
+  *Description: This progra assigns a random number to the variable n                                                                     
+
+  *and prints whether the number is positive, negative, or zero.                                                                          
+
+  *                                                                                                                                       
+
+  *Return: 0 on success                                                                                                                   
+
+  */
+
+int main(void)
+
+{
+  
+  int n;
+  
+  int ultimoDigito;
+  
+  srand(time(0));
+  
+  n = rand() - RAND_MAX / 2;
+  
+  ultimoDigito = n % 10;
+  
+  printf("Last digit of %d is %d ", n, ultimoDigito);
+  
+  if (ultimoDigito > 5)
+    
+    {
+      
+      printf("and is greater than 5\n");
+      
+    }
+  
+  else if (ultimoDigito == 0)
+    
+    {
+      
+      printf("and is 0\n");
+      
+    }
+  
+  else
+    
+    {
+      
+      printf("and is less than 6 and not 0\n");
+      
+    }
+  
+  return (0);
+  
+}fdfadfaadfadsfasdfasdfasdfadfasdfadsfasfsa
---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+char letra = 'a';  
+while (letra <= 'z')
+{   
+putchar(letra);    
+letra++;  
+}  
+putchar('\n');  
+return 0;  
+}
+
+
