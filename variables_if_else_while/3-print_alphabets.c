eaf4ac99697b41f631d12c8dfed58b8b321f8c26
JavierHRanero <javierherreraranero@gmail.com>
2023-06-21 23:07:04 -0700

    3.alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+*Description: This progra assigns a random number to the variable n
+*and prints whether the number is positive, negative, or zero.
+*
+*Return: 0 on success
+*/
+int main(void)
+{
+char letra = 'a';
+char letra2 = 'A';
+while (letra <= 'z')
+{
+putchar(letra);
+letra++;
+}
+while (letra2 <= 'Z')
+{
+putchar (letra2);
+letra2++;  
+} 
+putchar('\n');
+return (0);
+}
