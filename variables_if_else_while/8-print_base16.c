9a4d1ac74dccff40d9bcf76e21dd3d0fea40f368
JavierHRanero <javierherreraranero@gmail.com>
2023-06-21 23:51:00 -0700

    8.Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - Entry point of the program
+*
+*Description: This progra assigns a random number to the variable n
+*and prints whether the number is positive, negative, or zero.
+*
+*Return: 0 on success
+*/
+int main(void)
+{
+int numero;
+for (numero = '0'; numero <= 'f'; numero++)
+{
+if ((numero >='0'; && numero <='9') || (numero >= 'a' && numero <= 'f'))
+{
+putchar(numero);
+}
+}
+putchar('\n');
+return (0);
+}
