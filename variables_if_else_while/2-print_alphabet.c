d9ef009ac3c9bd2b2afa992e1ebc8b2c6ce362b4
JavierHRanero <javierherreraranero@gmail.com>
2023-06-21 23:01:03 -0700

    2.I sometimes suffer from insomnia

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+*main - Entry point of the program
+*
+*Description: This progra assigns a random number to the variable n
+*and prints whether the number is positive, negative, or zero.  
+*
+*Return: 0 on success
+*/
 int main(void)
 {
 char letra = 'a';
