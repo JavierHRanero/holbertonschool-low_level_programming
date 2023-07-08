38bc201c1f871575ed0a46fc3001e149b4548f4a
JavierHRanero <javierherreraranero@gmail.com>
2023-07-08 09:52:15 -0700

    5.Rev string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
-* _strstr - Entry point
-* @haystack: input
-* @needle: input
-* Return: Always 0 (Success)
+* rev_string - Reverses a string.
+* @s: The string to be reversed.
 */
-char *_strstr(char *haystack, char *needle)
+void rev_string(char *s)
 {
-for (; *haystack != '\0'; haystack++)
+int length = 0;
+int start = 0;
+int end = 0;
+char temp;
+/* Obtener la longitud de la cadena */
+while (s[length] != '\0')
+length++;
+end = length - 1;
+/* Intercambiar los caracteres desde el principio y el final */
+while (start < end)
 {
-char *l = haystack;
-char *p = needle;
-while (*l == *p && *p != '\0')
-{
-l++;
-p++;
+temp = s[start];
+s[start] = s[end];
+s[end] = temp;
+start++;
+end--;
 }
-if (*p == '\0')
-return (haystack);
-}
-return (0);
 }
