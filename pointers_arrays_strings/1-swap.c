603fc51a24c2e81701206b93a67e55ed7cbaaabf
JavierHRanero <javierherreraranero@gmail.com>
2023-07-08 09:37:41 -0700

    Programa 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,18 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* main - check the code
-*
-* Return: Always 0.
+* swap_int - swaps the values of two integers
+* @a: pointer to the first integer
+* @b: pointer to the second integer
 */
-int main(void)
+void swap_int(int *a, int *b)
 {
-int a;
-int b;
-a = 98;
-b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
-return (0);
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
 }
